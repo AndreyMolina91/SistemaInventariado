a819431369891e803d0908ff666c376f1ed086e3
Andrey Lema Molina <andrey_molina91@hotmail.com>
2021-11-01 18:09:42 -0300

    Modulo Inventariado iniciado - Front AccessDenied 404-NF

---
+++ InventorySystem.Models/BodegaProducto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Models
+{
+    public class BodegaProducto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Bodega")]
+        public int BodegaId { get; set; }
+
+        [ForeignKey("BodegaId")]
+        public Bodega Bodega { get; set; }
+
+        [Required]
+        [Display(Name ="Producto")]
+        public int ProductoId { get; set; }
+
+        [ForeignKey("ProductoId")]
+        public Producto Producto { get; set; }
+
+        [Required]
+        [Display(Name ="Cantidad")]
+        public int Cantidad { get; set; }
+    }
+}
---
+++ InventorySystem.Models/DetalleInventario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem.Models
+{
+    public class DetalleInventario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int InventarioId { get; set; }
+
+        [ForeignKey("InventarioId")]
+
+        [Required]
+        [Display(Name = "Producto")]
+        public int ProductoId { get; set; }
+
+        [ForeignKey("ProductoId")]
+        public Producto Producto { get; set; }
+
+        [Required]
+        [Display(Name ="Stock Anterior")]
+        public int StockAnterior { get; set; }
+
+        [Required]
+        [Display(Name = "Cantidad")]
+        public int Cantidad { get; set; }
+    }
+}
